01f346a31241e55b021e11ee890418f620fd71b4
Alex Kranias <alex.kranias@amd.com>
2024-10-02 16:52:58 -0500

    test: more accurate sliding window test

--- tests/test_sliding_window.py
+++ tests/test_sliding_window.py
@@ -6,22 +6,21 @@ BLOCK_M = 4
 BLOCK_N = 4
 
 head_dim = 1
-N_CTX_Q = 8
+N_CTX_Q = 4
 N_CTX_KV = 8
 
-window = (-2, 0)
+window = (0, 0)
 WINDOW_SIZE_LEFT = window[0]
 WINDOW_SIZE_RIGHT = window[1]
 
-qk = torch.randn(N_CTX_Q, N_CTX_KV) + 0
+qk = torch.randint(1, 9, (N_CTX_Q, N_CTX_KV))
 print("QK:\n", qk)
 print("WINDOW: ", window)
 
 for start_m in range(N_CTX_Q // BLOCK_M):
-    for start_n in range(N_CTX_KV // BLOCK_N):
-
+    for start_n in range(0, N_CTX_KV, BLOCK_N):
         row_idx = start_m * BLOCK_M + torch.arange(0, BLOCK_M)
-        col_idx = start_n * BLOCK_N + torch.arange(0, BLOCK_N)
+        col_idx = start_n + torch.arange(0, BLOCK_N)
 
         mask = torch.full((BLOCK_M, BLOCK_N), 1)
 
@@ -34,7 +33,7 @@ for start_m in range(N_CTX_Q // BLOCK_M):
             print("MASK:\n", mask)
 
             qk[row_idx[:, None], col_idx] = qk[row_idx[:, None], col_idx] * mask
-            print("ROW: ", row_idx, "COL: ", col_idx)
+            print("ROW: ", row_idx, "COL: ", col_idx, "COL_OFF: ", col_offset)
             print("MINI QK:\n", qk[row_idx[:, None], col_idx])
     
     print("QK after MASK:\n", qk)
