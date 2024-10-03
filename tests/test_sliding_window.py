67e12801f19c275cc0b976ff8713cd4656392f63
Alex Kranias <alex.kranias@amd.com>
2024-10-03 09:43:28 -0500

    test: added mask to BLOCK_N

--- tests/test_sliding_window.py
+++ tests/test_sliding_window.py
@@ -8,7 +8,7 @@ BLOCK_N = 64
 
 head_dim = 1
 N_CTX_Q = 1
-N_CTX_KV = 128
+N_CTX_KV = 57
 
 window = (-5, 0)
 WINDOW_SIZE_LEFT = window[0]
@@ -21,9 +21,9 @@ print("WINDOW: ", window)
 for start_m in range(math.ceil(N_CTX_Q / BLOCK_M)):
     for start_n in range(0, N_CTX_KV, BLOCK_N):
         row_idx = start_m * BLOCK_M + torch.arange(0, BLOCK_M) if N_CTX_Q >= (start_m+1) * BLOCK_M else start_m * BLOCK_M + torch.arange(0, N_CTX_Q % BLOCK_M)
-        col_idx = start_n + torch.arange(0, BLOCK_N)
+        col_idx = start_n + torch.arange(0, BLOCK_N) if N_CTX_KV >= (start_n + BLOCK_N) else start_n + torch.arange(0, N_CTX_KV % BLOCK_N)
 
-        mask = torch.full((BLOCK_M, BLOCK_N), 1) if N_CTX_Q >= (start_m+1) * BLOCK_M else torch.full((N_CTX_Q % BLOCK_M, BLOCK_N), 1)
+        mask = torch.full(((BLOCK_M if N_CTX_Q >= (start_m+1) * BLOCK_M else N_CTX_Q % BLOCK_M), (BLOCK_N if N_CTX_KV >= (start_n + BLOCK_N) else N_CTX_KV % BLOCK_N)), 1)
 
         col_offset = N_CTX_Q - N_CTX_KV
 
