1c0c99d41cc936404553fbc622f70cc7365752c5
Michael Melesse <micmelesse@gmail.com>
2025-02-18 08:06:38 -0800

    test and bench work compressed
    enable more tests
    
    match test
    
    add tests
    
    add more tests
    
    add nightly and do triton 3.2.0
    
    add deps for benching
    
    min diff with og test
    
    reset changes
    
    rm readme changes
    
    reduce splitkv cases
    
    enable deterministic, kvpacked, swap_sq_sk & disable local, bfloat
    
    increase timeout 720
    
    disable kvpacked
    
    skip flaky test
    
    be verbose
    
    skip config with 1
    
    n_groups
    
    use grad strides
    
    rename maxseqlen and nonvarlen input helper
    
    bench mark api directly
    
    min diffs

--- flash_attn/flash_attn_triton_amd/bench.py
+++ flash_attn/flash_attn_triton_amd/bench.py
@@ -1,12 +1,13 @@
 import argparse
 import torch
 import triton
+from flash_attn import flash_attn_func, flash_attn_qkvpacked_func, flash_attn_kvpacked_func, \
+    flash_attn_varlen_func, flash_attn_varlen_qkvpacked_func, flash_attn_varlen_kvpacked_func, \
+    flash_attn_with_kvcache
 from flash_attn.flash_attn_triton_amd.utils import (
-    MetaData,
-    input_helper,
+    nonvarlen_input_helper,
     varlen_input_helper,
 )
-from flash_attn.flash_attn_triton_amd.interface_torch import attention_prefill, attention_decode
 
 ARGS_TO_TORCH_DTYPE = {
     "fp16": torch.float16,
@@ -15,11 +16,21 @@ ARGS_TO_TORCH_DTYPE = {
 }
 
 FUNCTIONS = {
-    "prefill": attention_prefill,
-    "decode": attention_decode
+    "flash_attn": flash_attn_func,
+    # "flash_attn_qkvpacked": flash_attn_qkvpacked_func,
+    # "flash_attn_kvpacked": flash_attn_kvpacked_func,
+    "flash_attn_varlen": flash_attn_varlen_func,
+    # "flash_attn_varlen_qkvpacked": flash_attn_varlen_qkvpacked_func,
+    # "flash_attn_varlen_kvpacked": flash_attn_varlen_kvpacked_func,
+    "flash_attn_with_kvcache": flash_attn_with_kvcache,
 }
 
-def get_benchmark_configs(args, varlen=False):
+MODES = {
+        "fwd": "for forward pass only", 
+        "full": "for forward and backward pass"
+         }
+
+def get_benchmark_configs(args, is_varlen=False):
     """
     Returns benchmark configurations based on whether variable-length sequences are used.
     """
@@ -27,7 +38,7 @@ def get_benchmark_configs(args, varlen=False):
         hk = args.hq if not args.hk else args.hk
         sk = args.sq if not args.sk else args.sk
         return [(args.b, args.hq, hk, args.sq, sk)]
-    elif varlen:
+    elif is_varlen:
         return [
             (2, 16, 4, 1024, 1024),
             (8, 16, 2, 2048, 2048),
@@ -67,57 +78,88 @@ def get_benchmark_configs(args, varlen=False):
             (2, 8, 8, 8181, 1021),
         ]
 
-def gen_fn_inputs(fn_name, BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, device, layout, causal):
-    flops_per_matmul = 0
-
-    if fn_name.startswith("prefill"):
-        if layout == "thd":
-            q, k, v, input_metadata = varlen_input_helper(
-                BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, device=device)
-            for i in range(input_metadata.num_contexts):
-                seqlen_q = input_metadata.cu_seqlens_q[i + 1] - input_metadata.cu_seqlens_q[i]
-                seqlen_k = input_metadata.cu_seqlens_k[i + 1] - input_metadata.cu_seqlens_k[i]
-                flops_per_matmul += seqlen_q.item() * seqlen_k.item() * HQ * D_HEAD * 2
-        else:
-            q, k, v, input_metadata = input_helper(
-                BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, layout, device=device
-            )
-            flops_per_matmul = 2.0 * BATCH * HQ * N_CTX_Q * N_CTX_K * D_HEAD
-
-        if causal:
-            input_metadata.need_causal()
-
-        o = torch.empty_like(q)
-        input_data = (q, k, v, o, input_metadata)
-    elif fn_name.startswith("decode"):
-        q = torch.randn(
-            [BATCH, N_CTX_Q, HK, HQ // HK, D_HEAD],
-            device=device,
-            dtype=dtype,
-            requires_grad=False,
+def create_benchmark_fn(fn_name, BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, device, dropout_p, causal, mode):
+    if fn_name == "flash_attn":
+        q, k, v, _ = nonvarlen_input_helper(
+            BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, "bshd", device=device
         )
-        k = torch.randn(
-            [BATCH, N_CTX_K, HK, 1, D_HEAD],
-            device=device,
-            dtype=dtype,
-            requires_grad=False,
-        ).expand(-1, -1, -1, HQ // HK, -1)
-        v = torch.randn(
-            [BATCH, N_CTX_K, HK, 1, D_HEAD],
-            device=device,
-            dtype=dtype,
-            requires_grad=False,
-        ).expand(-1, -1, -1, HQ // HK, -1)
-        input_metadata = MetaData(sm_scale=1.3)
-        input_metadata.layout = "bsghd"
-        
-        # Adjust flops calculation if needed
-        flops_per_matmul = 2.0 * BATCH * HQ * N_CTX_Q * N_CTX_K * D_HEAD
+        def flash_attn_bench_fn():
+            out, lse, S_dmask = flash_attn_func(
+                q,
+                k,
+                v,
+                dropout_p,
+                causal=causal,
+                window_size=(-1, -1),
+                softcap=0.0,
+                alibi_slopes=None,
+                deterministic=False,
+                return_attn_probs=True,
+            )
+            if mode == "full":
+                do = torch.randn_like(out)
+                (
+                dq,
+                dk,
+                dv,
+                ) = torch.autograd.grad(out, (q, k, v), do)
 
-        input_data = (q, k, v, input_metadata)
+        return flash_attn_bench_fn
+       
+    elif fn_name == "flash_attn_varlen":
+        q_unpad, k_unpad, v_unpad, metadata = varlen_input_helper(
+            BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, device=device)
+        def flash_attn_varlen_bench_fn():
+            out_unpad, sm_lse, S_dmask = flash_attn_varlen_func(
+                q_unpad,
+                k_unpad,
+                v_unpad,
+                metadata.cu_seqlens_q,
+                metadata.cu_seqlens_k,
+                metadata.max_seqlen_q,
+                metadata.max_seqlen_k,
+                dropout_p,
+                causal=causal,
+                window_size=(-1, -1),
+                softcap=0.0 ,
+                alibi_slopes=None,
+                deterministic=False,
+                return_attn_probs=True,
+            )
+            if mode == "full":
+                do_unpad = torch.randn_like(out_unpad)
+                (
+                dq_unpad,
+                dk_unpad,
+                dv_unpad,
+                ) = torch.autograd.grad(out_unpad, (q_unpad, k_unpad, v_unpad), do_unpad)
+        return flash_attn_varlen_bench_fn
+    elif fn_name == "flash_attn_with_kvcache":
+        q, k_cache, v_cache, _ = nonvarlen_input_helper(
+            BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, "bshd", device=device
+        )
+        def flash_attn_with_kvcache_bench_fn():
+            out = flash_attn_with_kvcache(
+                q,
+                k_cache,
+                v_cache,
+                None,
+                None,
+                rotary_cos=None,
+                rotary_sin=None,
+                cache_seqlens=None,
+                cache_batch_idx=None,
+                cache_leftpad=None,
+                block_table=None,
+                causal=causal,
+                window_size=(-1, -1),
+                rotary_interleaved=False,
+                alibi_slopes=None,
+                num_splits=0,
+            )
+        return flash_attn_with_kvcache_bench_fn
     else:
-        raise ValueError("Unsupported benchmark function")
-    return input_data, flops_per_matmul
+        raise ValueError(f"{fn_name} is not supported for benchmarking")
 
 def run_benchmark(args, fn_name, fn, mode):
     """
@@ -125,85 +167,63 @@ def run_benchmark(args, fn_name, fn, mode):
     """
     print(f"Benchmarking {fn_name} in {mode} mode...")
 
+    # get configs
     dtype = ARGS_TO_TORCH_DTYPE[args.dtype]
     head_size = args.d if args.d else 128
     causal = args.causal
-    varlen = args.layout == "thd"
-    return_tflops = args.return_tflops
-    line_names = "TFLOPS" if return_tflops else "Time (ms)"
-
-    # Determine configurations
-    x_vals_list = get_benchmark_configs(args, varlen=varlen)
+    dropout_p = 0.0
+    configs = get_benchmark_configs(args, is_varlen=True if "varlen" in fn_name else False)
 
     # Setup benchmark configurations
     configs = [
         triton.testing.Benchmark(
             x_names=["BATCH", "HQ", "HK", "N_CTX_Q", "N_CTX_K"],
-            x_vals=x_vals_list,
+            x_vals=configs,
             line_arg="provider",
             line_vals=["triton"],
-            line_names=[line_names],
+            line_names=["Time (ms)"],
             styles=[("red", "-")],
             ylabel="ms",
-            plot_name=f"benchmark-{fn_name}-d{head_size}-layout{args.layout}-mode{mode}",
+            plot_name=f"benchmark-{fn_name}-{mode}",
             args={
                 "D_HEAD": head_size,
                 "dtype": dtype,
                 "causal": causal,
-                "mode": mode,
+                "dropout_p": dropout_p,
+                "mode": mode
             },
         )
     ]
 
     @triton.testing.perf_report(configs)
     def bench_function(
-        BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, causal, mode, provider, device="cuda"
+        BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, causal, dropout_p, mode, provider, device="cuda"
     ):
         warmup = 25
         rep = 100
-        flops_per_matmul = 0
 
-        # generate function inputs
-        fn_inputs, flops_per_matmul = gen_fn_inputs(
-            fn_name, BATCH, HQ, HK, N_CTX_Q, N_CTX_K, D_HEAD, dtype, device, args.layout, causal
-        )
-
-        # define the function to benchmark
-        if mode == "fwd":
-            benchmark_fn = lambda: fn(*fn_inputs)
-            total_flops = 2 * flops_per_matmul
-        elif mode == "bwd":
-            outputs = fn(*fn_inputs)
-            output = outputs[0]
-            grad_output = torch.randn_like(output)
-            benchmark_fn = lambda: output.backward(grad_output, retain_graph=True)
-            total_flops = 2 * flops_per_matmul * 2.5
-        else:
-            raise ValueError("Unsupported mode. Choose 'fwd' or 'bwd'.")
+        benchmark_fn = create_benchmark_fn(fn_name,
+                                           BATCH, 
+                                           HQ, 
+                                           HK, 
+                                           N_CTX_Q, 
+                                           N_CTX_K, 
+                                           D_HEAD, 
+                                           dtype, 
+                                           device,
+                                           causal,
+                                           dropout_p,
+                                           mode)
 
         if causal:
             total_flops *= 0.5
 
-        # Run the benchmark
+        # run the benchmark
         ms = triton.testing.do_bench(benchmark_fn, warmup=warmup, rep=rep)
-
-        if return_tflops:
-            return total_flops / ms * 1e-9
-        else:
-            return ms
+        return ms
 
     bench_function.run(save_path=".", print_data=True)
 
-def supported_layouts():
-    """
-    Returns a string describing the supported layouts.
-    """
-    return (
-        "bhsd: Q, K, V are individual tensors of [batch, num_heads, seqlen_q/k, head_size]\n"
-        "bshd: Q, K, V are individual tensors of [batch, seqlen_q/k, num_heads, head_size]\n"
-        "thd: Q, K, V are individual tensors of [total_q/k, num_heads, head_size]\n"
-        'This layout is sometimes called "varlen" or "grouped" layout.'
-    )
 
 def parse_args():
     """
@@ -218,36 +238,24 @@ def parse_args():
     parser.add_argument("-hk", type=int, default=0)
     parser.add_argument("-sq", type=int, default=0)
     parser.add_argument("-sk", type=int, default=0)
-    parser.add_argument(
-        "-equal_seqlens",
-        action="store_true",
-        default=False,
-        help="If specified, each context within the thd layout has same seqlen as sq and sk",
-    )
     parser.add_argument("-d", type=int, default=0)
     parser.add_argument("-causal", action="store_true", default=False)
     parser.add_argument("-dtype", default="fp16")
-    parser.add_argument("-return_tflops", action="store_true", default=False)
-    parser.add_argument(
-        "-layout",
-        type=str,
-        default="bhsd",
-        help=supported_layouts(),
-    )
     parser.add_argument(
         "-benchmark_fn",
         type=str,
         nargs="*",
         choices=FUNCTIONS.keys(),
-        help="Function(s) to benchmark: prefill, decode, or both",
+        help=f"Function(s) to benchmark: {FUNCTIONS.keys()}",
     )
+    mode_help_str = ". ".join(f'"{k}" {v.replace(" only", "")}' for k, v in MODES.items())
     parser.add_argument(
         "-mode",
         type=str,
         nargs='*',
-        default=["fwd", "bwd"],
-        choices=["fwd", "bwd"],
-        help="Mode(s) to run: 'fwd' for forward pass, 'bwd' for backward pass",
+        default= MODES.keys(),
+        choices= MODES.keys(),
+        help=f"Mode(s) to run: {mode_help_str}",
     )
     return parser.parse_args()
 
@@ -258,17 +266,13 @@ def main():
     args = parse_args()
 
     # Validate arguments
-    assert (
-        args.layout == "thd" or not args.equal_seqlens
-    ), "Equal sequence lengths arg must be used with the thd layout."
     args.custom_config = False
     if args.b or args.hq or args.hk or args.sq or args.sk or args.d:
         args.custom_config = True
         assert args.b and args.hq and args.sq and args.d, (
-            "If custom config is specified, please provide all of batch, "
-            "number of Q heads, Q sequence length, and head size."
+            "if custom config is specified, please provide batch, number of Q heads, Q sequence length, and head size."
         )
-    assert args.dtype in ARGS_TO_TORCH_DTYPE, "Only fp16, bf16 and fp32 types currently supported."
+    assert args.dtype in ARGS_TO_TORCH_DTYPE, "only fp16, bf16 and fp32 types currently supported."
 
     # determine the functions to benchmark
     if args.benchmark_fn is None or len(args.benchmark_fn) == 0:
@@ -276,13 +280,13 @@ def main():
     else:
         bench_fn_list = args.benchmark_fn
 
-    # benchmark functions
+    # run benchmarks
     for fn_name in bench_fn_list:
         if fn_name not in FUNCTIONS:
-            raise ValueError(f"Invalid benchmark function specified: {fn_name}")
+            raise ValueError(f"invalid benchmark function specified: {fn_name}")
         for mode in args.mode:
-            if fn_name == "decode" and mode == "bwd":
-                print(f"Decode kernel doesnot have a backward pass")
+            if fn_name in ["flash_attn_with_kvcache"] and mode == "full":
+                print(f"{fn_name} kernel does not have a backward pass")
                 continue
             run_benchmark(args, fn_name, FUNCTIONS[fn_name], mode)
 
